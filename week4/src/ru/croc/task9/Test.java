b9b13f7faa529f3739fd59506576b753d051a0e9
Katya959 <61267054+Katya959@users.noreply.github.com>
2021-11-10 17:52:32 +0300

    Добавлены комментарии в Test.java

--- week4/src/ru/croc/task9/Test.java
+++ week4/src/ru/croc/task9/Test.java
@@ -40,9 +40,11 @@ public class Test {
         }
         System.out.println(map1);
 
+        //Сортировка ключей в map1(где файлы с расширением .log и .trace)
         Object[] keys = map1.keySet().toArray();
         Arrays.sort(keys);
 
+        //При выводе сначала вывожу значение ключа, а потом через пробел все сообщения, который содержаться в ArrayList, полученном по данному ключу.
         for (Object key: keys) {
             System.out.print(key);
             for (String line: map1.get(key)) {
@@ -52,28 +54,45 @@ public class Test {
         }
     }
 
+    //Эта функция ищет в указанной директории файла с определенным расширением, которое передается вторым аргументом в функцию, а потом анализирует данные
+    //в этих файлах
     public static HashMap<Long, ArrayList<String>> findLogesInFilesWithExstension(String dir, String ext) throws IOException {
+        //В данный map я помещаю время сообщения и само сообщения.
+        //Я понимаю, что может быть одно и то же время у нескольких сообщений в файлах, поэтому так как я считываю все файла одновременно, у меня может 
+        //получиться, что время одно, а сообщений несколько. Поэтому я для записи сообщений создаю ArrayList, в который при повторяющемся времени будут
+        //записываться сообщения.
+        //Делаю это из-за того, что в map все ключи уникальные, поэтому пара с одинаковым ключом создать нельзя.
+        //Например: 10 message 10, 10 message15, 20 message20. Когда я считала данные строки, я получаю: 10 {message 10, message15}, 20 {message20}.
+        //Т. е. при повторяющеймся времени все сообщения будут сохранены!
         HashMap<Long, ArrayList<String>> map = new HashMap<>();
         File file = new File(dir);
         if(!file.exists()) System.out.println(dir + " папка не существует");
+        //Массив с файлами определенного расширения
         File[] listFiles = file.listFiles(new MyFileNameFilter(ext));
         if(listFiles.length == 0){
             System.out.println(dir + " не содержит файлов с расширением " + ext);
         }else{
+            //Прохожусь по всем найденным файлам данного расширения
             for (int i = 0; i < listFiles.length; i++) {
                 BufferedReader reader = new BufferedReader(new FileReader(listFiles[i]));
+                //Считываю по строчкам из каждого файла
                 String line = reader.readLine();
+                //Если она не пустая
                 while(line!=null){
+                    //Создаю log, в котором записано время сообщения и само сообщение типа String.
                     LogEntry log = new LogEntry(line);
+                    //Создается массив ArrayList строк, в который при наличии помещаются все сообщения из map с ключом log.time (массив сообщений).
                     ArrayList<String> itemsList = map.get(log.time);
 
-                    // if list does not exist create it
+                    // Если массив сообщений пустой, значит, с данным временем (ключом) сообщения еще не были найдены.
+                    //В данном случае я создаю ArrayList для значений данного ключа, добавляю в него сообщение log.message.
                     if(itemsList == null) {
                         itemsList = new ArrayList<String>();
                         itemsList.add(log.message);
+                        //В map добавляю пару: ключ (значение времени log.time), а также значения (itemsList).
                         map.put(log.time, itemsList);
                     } else {
-                        // add if item is not already in list
+                        // Если же ArrayList с сообщениями по данному ключу не пустой, тогда я в данный ArrayList добавляю к уже имеющимся сообщениям еще одно.
                         if(!itemsList.contains(log.message)) itemsList.add(log.message);
                     }
                     line = reader.readLine();
